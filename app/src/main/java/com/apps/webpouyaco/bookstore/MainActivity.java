bd2e11a35685df8801d30f8b1ecf1ebd3186fa96
ashkanfakhr <fakhr.ashkan@gmail.com>
2016-07-05 13:36:49 +0430

    search box 1

--- app/src/main/java/com/apps/webpouyaco/bookstore/MainActivity.java
+++ app/src/main/java/com/apps/webpouyaco/bookstore/MainActivity.java
@@ -1,7 +1,9 @@
 package com.apps.webpouyaco.bookstore;
 
 import android.app.SearchManager;
+import android.content.Context;
 import android.content.Intent;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.SearchView;
@@ -10,9 +12,21 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ExpandableListView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity
+        implements SearchView.OnQueryTextListener, SearchView.OnCloseListener {
+
+    private SearchManager searchManager;
+    private android.widget.SearchView searchView;
+    private MyExpandableListAdapter listAdapter;
+    private ExpandableListView myList;
+    private ArrayList<ParentRow> parentList = new ArrayList<ParentRow>();
+    private ArrayList<ParentRow> showTheseParentList = new ArrayList<ParentRow>();
+    private MenuItem searchItem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,18 +44,70 @@ public class MainActivity extends AppCompatActivity {
 //            Toast.makeText(MainActivity.this, query, Toast.LENGTH_LONG).show();
 //        }
 
+        searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+
+        parentList = new ArrayList<ParentRow>();
+        showTheseParentList = new ArrayList<ParentRow>();
+
+        //the app will crash if display list is not called here.
+        displayList();
+
+        //this expands the list of continents.
+        expandAll();
+
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
 
-//        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        searchItem = menu.findItem(R.id.action_search);
+        searchView = (android.widget.SearchView) MenuItemCompat.getActionView(searchItem);
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+        searchView.setIconifiedByDefault(false);
+        searchView.setOnQueryTextListener(this);
+        searchView.setOnCloseListener(this);
+        searchView.requestFocus();
 
 //        SearchView searchView = (SearchView) menu.findItem(R.id.searchable).getActionView();
 //        SearchManager searchManager = (SearchManager) getSystemService(SEARCH_SERVICE);
 //        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
 
         return super.onCreateOptionsMenu(menu);
+    }
+
+    private void loadData() {
+        ArrayList<ChildRow> childRows = new ArrayList<ChildRow>();
+        ParentRow parentRow = null;
+
+        childRows.add(new ChildRow(R.mipmap.ic_launcher, "test text"));
+        childRows.add(new ChildRow(R.mipmap.ic_launcher, "test text2"));
+        parentRow = new ParentRow("first group", childRows);
+        parentList.add(parentRow);
+
+        childRows = new ArrayList<ChildRow>();
+
+        childRows.add(new ChildRow(R.mipmap.ic_launcher, "test text3"));
+        childRows.add(new ChildRow(R.mipmap.ic_launcher, "test text4"));
+        parentRow = new ParentRow("second group", childRows);
+        parentList.add(parentRow);
+    }
+
+    private void expandAll() {
+        int count = listAdapter.getGroupCount();
+        for (int i = 0; i < count; i++) {
+            myList.expandGroup(i);
+        }
+    }
+
+    private void displayList() {
+        loadData();
+
+        myList = (ExpandableListView) findViewById(R.id.expandableListView_search);
+        listAdapter = new MyExpandableListAdapter(MainActivity.this, parentList);
+
+        myList.setAdapter(listAdapter);
     }
 
     @Override
@@ -55,5 +121,26 @@ public class MainActivity extends AppCompatActivity {
 
     public void shopBasketClick(View view) {
 
+    }
+
+    @Override
+    public boolean onClose() {
+        listAdapter.filterData("");
+        expandAll();
+        return false;
+    }
+
+    @Override
+    public boolean onQueryTextSubmit(String query) {
+        listAdapter.filterData(query);
+        expandAll();
+        return false;
+    }
+
+    @Override
+    public boolean onQueryTextChange(String newText) {
+        listAdapter.filterData(newText);
+        expandAll();
+        return false;
     }
 }
--- app/src/main/java/com/apps/webpouyaco/bookstore/MyExpandableListAdapter.java
+++ app/src/main/java/com/apps/webpouyaco/bookstore/MyExpandableListAdapter.java
@@ -1,9 +1,13 @@
 package com.apps.webpouyaco.bookstore;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseExpandableListAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -61,16 +65,82 @@ public class MyExpandableListAdapter extends BaseExpandableListAdapter {
 
     @Override
     public View getGroupView(int groupPosition, boolean isExpanded, View convertView, ViewGroup parent) {
-        return null;
+
+        ParentRow parentRow = (ParentRow) getGroup(groupPosition);
+        if (convertView == null) {
+            LayoutInflater layoutInflater = (LayoutInflater)
+                    context.getSystemService(context.LAYOUT_INFLATER_SERVICE);
+            convertView = layoutInflater.inflate(R.layout.parent_row, null);
+        }
+
+        TextView heading = (TextView) convertView.findViewById(R.id.parent_text);
+
+        heading.setText(parentRow.getName().trim());
+        return convertView;
     }
 
     @Override
-    public View getChildView(int groupPosition, int childPosition, boolean isLastChild, View convertView, ViewGroup parent) {
-        return null;
+    public View getChildView(int groupPosition, int childPosition, boolean isLastChild,
+                             View convertView, ViewGroup parent) {
+
+        ChildRow childRow = (ChildRow) getChild(groupPosition, childPosition);
+        if (convertView == null) {
+            LayoutInflater layoutInflater = (LayoutInflater)
+                    context.getSystemService(context.LAYOUT_INFLATER_SERVICE);
+            convertView = layoutInflater.inflate(R.layout.child_row, null);
+        }
+
+        ImageView childIcon = (ImageView) convertView.findViewById(R.id.child_icon);
+
+        //icon should be created and used here
+        childIcon.setImageResource(R.mipmap.ic_launcher);
+
+        final TextView childText = (TextView) convertView.findViewById(R.id.child_text);
+        childText.setText(childRow.getText().trim());
+
+        final View finalConvertView = convertView;
+        childText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            //replace my own code here instead of Toast
+            // (opening activity, adding text, changing icon, ...)
+            public void onClick(View v) {
+                Toast.makeText(finalConvertView.getContext()
+                        , childText.getText()
+                        , Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        return convertView;
     }
 
     @Override
     public boolean isChildSelectable(int groupPosition, int childPosition) {
-        return false;
+        return true;
+    }
+
+    public void filterData(String query) {
+        query = query.toLowerCase();
+        parentRowList.clear();
+
+        if (query.isEmpty()) {
+            parentRowList.addAll(originalList);
+        } else {
+            for (ParentRow parentRow : originalList) {
+                ArrayList<ChildRow> childList = parentRow.getChildList();
+                ArrayList<ChildRow> newList = new ArrayList<ChildRow>();
+
+                for (ChildRow childRow : childList) {
+                    if (childRow.getText().toLowerCase().contains(query)) {
+                        newList.add(childRow);
+                    }
+                } //end for (ChildRow childRow : childList)
+                if (newList.size() > 0) {
+                    ParentRow nParentRow = new ParentRow(parentRow.getName(), newList);
+                    parentRowList.add(nParentRow);
+                }
+            } // end for (ParentRow parentRow : originalList)
+        } // end else
+
+        notifyDataSetChanged();
     }
 }
