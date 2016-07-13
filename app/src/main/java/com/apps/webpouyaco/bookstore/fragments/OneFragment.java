4b6c3ead6d45905d72717250e0177461f38d0235
ashkanfakhr <fakhr.ashkan@gmail.com>
2016-07-13 13:09:14 +0430

    commit from work place 7/13/2016 1:00 pm
    volley making json array requests

--- app/src/main/java/com/apps/webpouyaco/bookstore/fragments/OneFragment.java
+++ app/src/main/java/com/apps/webpouyaco/bookstore/fragments/OneFragment.java
@@ -1,15 +1,26 @@
 package com.apps.webpouyaco.bookstore.fragments;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.JsonArrayRequest;
 import com.apps.webpouyaco.bookstore.R;
+import com.apps.webpouyaco.bookstore.volley.AppController;
+
+import org.json.JSONArray;
 
 
 public class OneFragment extends Fragment{
+
+    private static final String TAG = "TAG";
 
     public OneFragment() {
         // Required empty public constructor
@@ -17,6 +28,34 @@ public class OneFragment extends Fragment{
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
+
+        //making json array request
+        String tag_json_arry = "json_array_req";
+
+        String url = "http://api.androidhive.info/volley/person_array.json";
+
+        ProgressDialog pDialog = new ProgressDialog(this);
+        pDialog.setMessage("Loading...");
+        pDialog.show();
+
+        JsonArrayRequest req = new JsonArrayRequest(url,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        Log.d(TAG, response.toString());
+                        pDialog.hide();
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                VolleyLog.d(TAG, "Error: " + error.getMessage());
+                pDialog.hide();
+            }
+        });
+
+// Adding request to request queue
+        AppController.getInstance().addToRequestQueue(req, tag_json_arry);
+
         super.onCreate(savedInstanceState);
     }
 
